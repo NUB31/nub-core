116e4344d2981416b60a43fbddd633c784963906
nub31 <oliverlhs@outlook.com>
2025-01-25 23:32:36 +0100

    init

---
+++ Nub.Lib/Nub.Lib/Optional.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Nub.Lib;
+
+public readonly struct Optional<TValue>
+{
+    public static Optional<TValue> Empty() => new();
+
+    public static Optional<TValue> OfNullable(TValue? value)
+    {
+        return value ?? Empty();
+    }
+
+    public Optional()
+    {
+        Value = default;
+        HasValue = false;
+    }
+
+    public Optional(TValue value)
+    {
+        Value = value;
+        HasValue = true;
+    }
+
+    public TValue? Value { get; }
+
+    [MemberNotNullWhen(true, nameof(Value))]
+    public bool HasValue { get; }
+
+    public static implicit operator Optional<TValue>(TValue value) => new(value);
+    public static implicit operator bool(Optional<TValue> value) => value.HasValue;
+}
